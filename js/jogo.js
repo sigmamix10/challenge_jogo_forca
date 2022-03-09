eb74333f9b6f84bf6acac6526c11572d164f1cb9
sigmamix10 <94759949+sigmamix10@users.noreply.github.com>
2022-03-09 15:51:49 -0300

    Projeto Jogo da Forca em Javascript

---
+++ js/jogo.js
@@ -0,0 +1,98 @@
+var palavras = [
+  'ARROZ',
+  'BORBOLETA',
+  'CARRO',
+  'MACARRAO',
+  'PASSARO',
+  'OVO',
+  'UVA',
+  'COMPUTADOR',
+  'PROGRAMACAO',
+  'ALURA',
+  'JAVASCRIPT',
+  'TECLADO',
+]
+
+let resposta = '';
+let tentativas = 6;
+let erros = 0;
+let letraCorreta = [];
+let verificaPalavra = null;
+
+function sorteiaPalavra() {
+  resposta = palavras[Math.floor(Math.random() * palavras.length)];
+}
+
+function criarBotoes() {
+  let botoes = 'ABCDEFGHIJKLMNOPQRSTUVXWZ'.split('').map(letra =>
+    `
+      <button
+        class="btn btn-lg btn-dark m-1"
+        id='` + letra + `'
+        onClick="letraEscolhida('` + letra + `')"
+      >
+        ` + letra + `
+      </button>
+    `).join('');
+
+  document.getElementById('keyboard').innerHTML = botoes;
+}
+
+function letraEscolhida(escolheLetra) {
+  letraCorreta.indexOf(escolheLetra) === -1 ? letraCorreta.push(escolheLetra) : null;
+  document.getElementById(escolheLetra).setAttribute('disabled', true);
+
+  if (resposta.indexOf(escolheLetra) >= 0) {
+    palavraCorreta();
+    verificaVitoria();
+  } else if (resposta.indexOf(escolheLetra) === -1) {
+    erros++;
+    atualizaErros();
+    verificaDerrota();
+    atualizaImage();
+  }
+}
+
+function atualizaImage() {
+  document.getElementById('jogoImage').src = './images/' + erros + '.png';
+}
+
+function verificaVitoria() {
+  if (verificaPalavra === resposta) {
+    document.getElementById('keyboard').innerHTML = 'Parabéns! Você Venceu!!!';
+  }
+}
+
+function verificaDerrota() {
+  if (erros === tentativas) {
+    document.getElementById('palavraVerificada').innerHTML = 'A Palavra Correta é:  ' + resposta;
+    document.getElementById('keyboard').innerHTML = 'Você Perdeu! Tente Novamente!';
+  }
+}
+
+function palavraCorreta() {
+  verificaPalavra = resposta.split('').map(letra => (letraCorreta.indexOf(letra) >= 0 ? letra : " ___ ")).join('');
+
+  document.getElementById('palavraVerificada').innerHTML = verificaPalavra;
+}
+
+function atualizaErros() {
+  document.getElementById('erros').innerHTML = erros;
+}
+
+function reset() {
+  erros = 0;
+  letraCorreta = [];
+  document.getElementById('jogoImage').src = './images/0.png';
+
+  sorteiaPalavra();
+  palavraCorreta();
+  atualizaErros();
+  criarBotoes();
+}
+
+document.getElementById('tentativas').innerHTML = tentativas;
+
+sorteiaPalavra();
+criarBotoes();
+palavraCorreta();
